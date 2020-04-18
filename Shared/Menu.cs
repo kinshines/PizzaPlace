e98def095800df33f58f1b9140c98434d6665360
kinshines <1176807665@qq.com>
2020-04-18 23:17:00 +0800

    first commit

---
+++ Shared/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaPlace.Shared
+{
+    public class Menu
+    {
+        public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+        public Pizza GetPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
