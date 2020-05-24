3b7da3ffeaee92cd4280cfd4a97d2f4afb677dad
kinshines <1176807665@qq.com>
2020-05-24 18:20:36 +0800

    'wasm3.2.0'

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using PizzaPlace.Shared;
 using PizzaPlace.Client.Services;
+using System.Net.Http;
 
 namespace PizzaPlace.Client
 {
@@ -16,7 +17,7 @@ namespace PizzaPlace.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<IMenuService, MenuService>();
             builder.Services.AddTransient<IOrderService, OrderService>();
             builder.Services.AddSingleton<State>();
